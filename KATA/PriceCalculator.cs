8fa5d5c26aff297002366439720779c65f3db063
OthmanSO <othman.subhi.123@gmail.com>
2022-04-11 18:05:43 +0300

    Add AND REFACTOR PriceCalculator Add Reporting
    -Add reporting fnunctionality
    -Add test cases to test the reporting
    -Refactor the PriceAfterDiscount and PriceAfterTax to be TaxAmount and DiscountAmount
    -Fix any related functionalities depending on the earlier mentioned functionalities
    -Fix test cases to test the new behaviour to the functions

--- KATA/PriceCalculator.cs
+++ KATA/PriceCalculator.cs
@@ -4,11 +4,14 @@
     {
         public double PriceBeforeTax(Product p) => p.price;
 
-        public double PriceAfterTax(Product prod, int taxRate) => PriceBeforeTax(prod) + prod.TaxValue(taxRate);
+        public double TaxAmount(Product prod, int taxRate) => prod.TaxValue(taxRate);
 
 
-        public double PriceAfterDiscount(Product prod, double discountRate) => prod.price - prod.DiscountValue(discountRate);
+        public double DiscountAmount(Product prod, double discountRate) => prod.DiscountValue(discountRate);
 
-        public object FinalProductPrice(Product product, int discountRate, int taxRate) => PriceAfterTax(product, taxRate) - product.DiscountValue(discountRate);
+        public object FinalProductPrice(Product product, int discountRate, int taxRate) => PriceBeforeTax(product) + TaxAmount(product, taxRate) - DiscountAmount(product,discountRate);
+
+        public object ReportFinalProductPriceAndDiscountamount(Product product, int discountRate, int taxRate)
+            => $"Price = ${this.FinalProductPrice(product, discountRate, taxRate)}, Discount amount = ${DiscountAmount(product,discountRate)}";
     }
 }
--- Price_Calculator_Testing/UnitTest1.cs
+++ Price_Calculator_Testing/UnitTest1.cs
@@ -19,33 +19,43 @@ namespace Price_Calculator_Testing
         }
 
         [Theory]
-        [InlineData(20, 24.30, "The Little Prince", 12345, 20.25)]
-        [InlineData(21, 24.50, "The Little Prince", 12345, 20.25)]
-        public void PriceAfterTax_InputProduct_ReturnDoublePrice(int taxRate, double expected, string name, int UPC, double price)
+        [InlineData(20, 4.05, "The Little Prince", 12345, 20.25)]
+        [InlineData(21, 4.25, "The Little Prince", 12345, 20.25)]
+        public void TaxAmount_InputProduct_ReturnDoubleTaxAmount(int taxRate, double expected, string name, int UPC, double price)
         {
             PriceCalculator pc = new PriceCalculator();
             var prod = GenerateProduct(name, UPC, price);
-            double actual = pc.PriceAfterTax(prod, taxRate);
+            double actual = pc.TaxAmount(prod, taxRate);
             actual.Should().Be(expected);
         }
 
         [Theory]
-        [InlineData(15, 17.21, "The Little Prince", 12345, 20.25)]
-        public void PriceAfterDiscount_InputProduct_outputDoublePriceAfterDiscount(int discountRate, double expected, string name, int UPC, double price)
+        [InlineData(15, 3.04, "The Little Prince", 12345, 20.25)]
+        public void DiscountAmount_InputProduct_outputDoubleDiscountAmount(int discountRate, double expected, string name, int UPC, double price)
         {
             var pc = new PriceCalculator();
             var product = GenerateProduct(name, UPC, price);
-            var actual = pc.PriceAfterDiscount(product, discountRate);
+            var actual = pc.DiscountAmount(product, discountRate);
             actual.Should().Be(expected);
         }
 
         [Theory]
         [InlineData(20, 15, 21.26, "The Little Prince", 12345, 20.25)]
-        public void PriceAfterTaxAndDiscount_InputProduct_outputDoublePriceAfterTaxAndDiscount(int taxRate, int discountRate, double expected, string name, int UPC, double price)
+        public void PriceAfterTaxAndDiscount_InputProductIntTaxINtDiscount_outputDoublePriceAfterTaxAndDiscount(int taxRate, int discountRate, double expected, string name, int UPC, double price)
         {
             var pc = new PriceCalculator();
             var product = GenerateProduct(name, UPC, price);
             var actual = pc.FinalProductPrice(product, discountRate, taxRate);
+            actual.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(20, 15, "Price = $21.26, Discount amount = $3.04", "The Little Prince", 12345, 20.25)]
+        public void ReportingPriceAfterTaxAndDiscount_InputProduct_outputStringFinalPriceAndDiscountAount(int taxRate, int discountRate, string expected, string name, int UPC, double price)
+        {
+            var pc = new PriceCalculator();
+            var product = GenerateProduct(name, UPC, price);
+            var actual = pc.ReportFinalProductPriceAndDiscountamount(product, discountRate, taxRate);
             actual.Should().Be(expected);
         }
 
